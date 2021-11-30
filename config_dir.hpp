2a48c1eae778745d1abb81fcdf8d60c18b8e5b90
cedric <cedric.perottet@outlook.com>
2021-11-30 09:58:04 +0000

    Added project directory in list_include_dirs for integration as submodule.

--- config_dir.hpp
+++ config_dir.hpp
@@ -3,6 +3,6 @@
 namespace pgi_test {
 
 // the following path will be set automatically during building (do not copy this file manually!)
-static const std::string pgi_config_dir = "/app/config/";
+static const std::string pgi_config_dir = "/app/src/pgi/config/";
 
 }  // namespace pgi_test
