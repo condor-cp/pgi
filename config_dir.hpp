ce4ee8b6c55ce06146327e0a19a1aa5e811069de
cedric <cedric.perottet@outlook.com>
2021-11-30 10:01:59 +0000

    gitignore config_dir.hpp

--- config_dir.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace pgi_test {
-
-// the following path will be set automatically during building (do not copy this file manually!)
-static const std::string pgi_config_dir = "/app/src/pgi/config/";
-
-}  // namespace pgi_test
